55119f77fe17e7f54fb17c09e5b509c60d15421e
Jorge Alexandre Balula <46543868+jorgealexandreb@users.noreply.github.com>
2021-03-24 17:33:18 +0000

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -9,9 +9,9 @@ function fibonacci(n) {
         fib[0] = 0;
         fib[1] = 1;    
     }
-        for (var i=2; i<n; i++) {
-            //fib generator;
-            fib[i] = fib[i-2] + fib[i-1];  
+    for (var i=2; i<n; i++) {
+         //fib generator;
+        fib[i] = fib[i-2] + fib[i-1];  
     }
     return fib;
 }
